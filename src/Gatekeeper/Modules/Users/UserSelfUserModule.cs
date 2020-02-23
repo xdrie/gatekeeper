6ac05aba5f71d74a2f1b89993cf3aa8290a38bf3
xdrie <xdrie@alt.icu>
2020-02-23 13:17:04 -0800

    rename AuthenticatedUserModule

--- src/Gatekeeper/Modules/Users/UserSelfUserModule.cs
+++ src/Gatekeeper/Modules/Users/UserSelfUserModule.cs
@@ -4,8 +4,8 @@ using Gatekeeper.Models.Identity;
 using Gatekeeper.OpenApi.Users;
 
 namespace Gatekeeper.Modules.Users {
-    public class UserSelfModule : AuthenticatedModule {
-        public UserSelfModule(SContext serverContext) : base("/u", serverContext) {
+    public class UserSelfUserModule : AuthenticatedUserModule {
+        public UserSelfUserModule(SContext serverContext) : base("/u", serverContext) {
             Get<GetMyself>("/me", async (req, res) => { await res.Negotiate(new AuthenticatedUser(currentUser)); });
         }
     }
