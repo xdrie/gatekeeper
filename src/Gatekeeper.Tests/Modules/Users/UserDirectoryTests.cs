d26166b990f51bee6c3792896dc793c339209c00
xdrie <xdrie@alt.icu>
2020-02-24 12:46:26 -0800

    add test for user directory

---
+++ src/Gatekeeper.Tests/Modules/Users/UserDirectoryTests.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using Gatekeeper.Models.Identity;
+using Gatekeeper.Tests.Base;
+using Gatekeeper.Tests.Meta;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Gatekeeper.Tests.Modules.Users {
+    [Collection(UserTestCollection.KEY)]
+    public class UserDirectoryTests {
+        private readonly UserTestFixture fx;
+
+        public UserDirectoryTests(UserTestFixture fixture) {
+            fx = fixture;
+        }
+
+        [Fact]
+        public async Task canFetchMeFromDirectory() {
+            await fx.initialize();
+            var client = fx.getAuthedClient();
+
+            // check me page
+            var resp = await client.GetAsync($"/a/u/{fx.username}");
+            resp.EnsureSuccessStatusCode();
+            var data = JsonConvert.DeserializeObject<PublicUser>(await resp.Content.ReadAsStringAsync());
+            Assert.Equal(fx.username, data.username);
+        }
+    }
+}
