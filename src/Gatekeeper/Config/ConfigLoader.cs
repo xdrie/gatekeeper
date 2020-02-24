1226da14205d898fd14560f27268371a00f589a8
xdrie <xdrie@alt.icu>
2020-02-23 23:42:15 -0800

    add more syntax sugar for config binding

--- src/Gatekeeper/Config/ConfigLoader.cs
+++ src/Gatekeeper/Config/ConfigLoader.cs
@@ -24,25 +24,31 @@ namespace Gatekeeper.Config {
             return (T) table[rename(field)];
         }
 
+        public static void bindField<T>(this TomlTable table, ref T target, string fieldName) {
+            if (table.ContainsKey(fieldName)) {
+                target = table.getField<T>(fieldName);
+            }
+        }
+
         public static SConfig readDocument(TomlTable tb) {
             var cfg = new SConfig();
 
             var server = tb.getField<TomlTable>(nameof(cfg.server));
 #if DEBUG
-            cfg.server.development = server.getField<bool>(nameof(cfg.server.development));
+            server.bindField(ref cfg.server.development, nameof(cfg.server.development));
 #endif
 
-            var apps = (TomlTableArray) tb[rename(nameof(cfg.apps))];
+            var apps = tb.getField<TomlTableArray>(nameof(cfg.apps));
             foreach (var app in apps) {
                 cfg.apps.Add(new SConfig.RemoteApp {
                     name = app.getField<string>(nameof(SConfig.RemoteApp.name))
                 });
             }
 
-            var logging = (TomlTable) tb[rename(nameof(cfg.logging))];
-            cfg.logging.logLevel = logging.getField<SLogger.LogLevel>(nameof(cfg.logging.logLevel));
-            cfg.logging.aspnetVerboseLogging = logging.getField<bool>(nameof(cfg.logging.aspnetVerboseLogging));
-            cfg.logging.databaseLogging = logging.getField<bool>(nameof(cfg.logging.databaseLogging));
+            var logging = tb.getField<TomlTable>(nameof(cfg.logging));
+            logging.bindField(ref cfg.logging.logLevel, nameof(cfg.logging.logLevel));
+            logging.bindField(ref cfg.logging.aspnetVerboseLogging, nameof(cfg.logging.aspnetVerboseLogging));
+            logging.bindField(ref cfg.logging.databaseLogging, nameof(cfg.logging.databaseLogging));
 
             return cfg;
         }
