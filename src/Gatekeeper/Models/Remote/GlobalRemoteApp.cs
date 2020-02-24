bd2eb80c38be3040e7af584f7b5143de3d436bd4
xdrie <xdrie@alt.icu>
2020-02-24 00:01:45 -0800

    issue tokens for apps

--- src/Gatekeeper/Models/Remote/GlobalRemoteApp.cs
+++ src/Gatekeeper/Models/Remote/GlobalRemoteApp.cs
@@ -4,6 +4,6 @@ using Gatekeeper.Config;
 namespace Gatekeeper.Models.Remote {
     public class GlobalRemoteApp : SConfig.RemoteApp {
         public override string name => "global";
-        public override List<string> layers => new List<string> { "*" };
+        public override List<string> paths => new List<string> { "*" };
     }
 }
--- src/Gatekeeper/Modules/Provider/AppAuthenticationModule.cs
+++ src/Gatekeeper/Modules/Provider/AppAuthenticationModule.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Linq;
+using System.Net;
 using Carter.Request;
 using Gatekeeper.Config;
 using Gatekeeper.Models.Identity;
+using Gatekeeper.Models.Responses;
+using Hexagon.Services.Serialization;
 
 namespace Gatekeeper.Modules.Provider {
     public class AppAuthenticationModule : AuthenticatedUserModule {
@@ -8,6 +13,34 @@ namespace Gatekeeper.Modules.Provider {
             Get("/token/{appId}", async (req, res) => {
                 var appId = req.RouteValues.As<string>("appId");
                 // check config for app layers
+                var appDef = serverContext.config.apps.FirstOrDefault(x => x.name == appId);
+                if (appDef == null) {
+                    res.StatusCode = (int) HttpStatusCode.NotFound;
+                    return;
+                }
+                // check if any permission grants app access
+                var grantedScope = default(string);
+                foreach (var permission in currentUser.permissions) {
+                    var permissionScope = new AccessScope(permission.path);
+                    foreach (var appPath in appDef.paths) {
+                        var appScope = new AccessScope(appPath);
+                        if (appScope.atLeast(permissionScope)) { // if permission grants app
+                            grantedScope = permissionScope.path;
+                        }
+                    }
+                }
+
+                if (grantedScope == null) {
+                    res.StatusCode = (int) HttpStatusCode.Unauthorized;
+                    return;
+                }
+                
+                // issue a new token
+                // TODO: configurable timespan
+                var token = serverContext.tokenAuthenticator.issue(grantedScope, TimeSpan.FromDays(7));
+
+                res.StatusCode = (int) HttpStatusCode.Created;
+                await res.respondSerialized(token);
             });
         }
     }
