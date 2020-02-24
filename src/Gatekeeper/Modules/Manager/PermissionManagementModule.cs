d7d3a364645654124b98910dd35f3594c34c1bf0
xdrie <xdrie@alt.icu>
2020-02-24 13:04:06 -0800

    add update route on permission management

--- src/Gatekeeper/Modules/Manager/PermissionManagementModule.cs
+++ src/Gatekeeper/Modules/Manager/PermissionManagementModule.cs
@@ -1,7 +1,21 @@
+using System.Net;
+using Carter.ModelBinding;
+using Carter.Response;
 using Gatekeeper.Config;
+using Gatekeeper.Models.Requests;
 
 namespace Gatekeeper.Modules.Manager {
     public class PermissionManagementModule : AdminModule {
-        public PermissionManagementModule(string path, SContext serverContext) : base(path, serverContext) { }
+        public PermissionManagementModule(SContext serverContext) : base("/perms", serverContext) {
+            Patch("/update", async (req, res) => {
+                var updateReq = await req.BindAndValidate<UpdatePermissionRequest>();
+                if (!updateReq.ValidationResult.IsValid) {
+                    res.StatusCode = (int) HttpStatusCode.UnprocessableEntity;
+                    await res.Negotiate(updateReq.ValidationResult.GetFormattedErrors());
+                }
+                
+                // TODO: fetch the user and update their permissions
+            });
+        }
     }
 }
