30623ad28e70b9eef9ea4e4804c7b238de6e3d85
xdrie <xdrie@alt.icu>
2020-04-05 14:04:49 -0700

    fix meta module

--- src/Gatekeeper.Server/Modules/Meta/MetaModule.cs
+++ src/Gatekeeper.Server/Modules/Meta/MetaModule.cs
@@ -7,7 +7,10 @@ namespace Gatekeeper.Server.Modules.Meta {
     public class MetaModule : ApiModule {
         public MetaModule(SContext serverContext) : base("/meta", serverContext) {
             Get<GetMeta>("/", async (req, res) => {
-                await res.respondSerialized(new ServerMetadata());
+                await res.respondSerialized(new ServerMetadata {
+                    name = SConfig.SERVER_NAME,
+                    version = SConfig.VERSION
+                });
             });
         }
     }
