b3bc540749f088e28001fbc65135c784601e9401
xdrie <xdrie@alt.icu>
2020-04-05 00:24:27 -0700

    ensure that requested groups exist

--- src/Gatekeeper/Modules/Manager/GroupManagementModule.cs
+++ src/Gatekeeper/Modules/Manager/GroupManagementModule.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Net;
 using Gatekeeper.Config;
 using Gatekeeper.Models.Access;
@@ -14,13 +15,22 @@ namespace Gatekeeper.Modules.Manager {
                 if (!validatedReq.isValid) return;
                 var updateReq = validatedReq.request;
 
-                // fetch the user and update their permissions
+                // ensure all the groups exist
+                foreach (var group in updateReq.groups) {
+                    if (serverContext.config.groups.All(x => x.name != group)) {
+                        res.StatusCode = (int) HttpStatusCode.NotFound;
+                        return;
+                    }
+                }
+
+                // fetch the user
                 var user = serverContext.userManager.findByUuid(updateReq.userUuid);
                 if (user == null) {
                     res.StatusCode = (int) HttpStatusCode.NotFound;
                     return;
                 }
 
+                // update their group memberships
                 var updateType =
                     Enum.Parse<Group.UpdateType>(updateReq.type, true);
                 foreach (var group in updateReq.groups) {
