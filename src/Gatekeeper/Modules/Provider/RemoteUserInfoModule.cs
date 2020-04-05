00c2d9dc1702f6ef1a66ab61e0ba69e400525ffb
xdrie <xdrie@alt.icu>
2020-04-04 18:32:45 -0700

    rename /a/app to /a/remote

--- src/Gatekeeper/Modules/Provider/RemoteUserInfoModule.cs
+++ src/Gatekeeper/Modules/Provider/RemoteUserInfoModule.cs
@@ -5,7 +5,7 @@ using Gatekeeper.OpenApi.App;
 
 namespace Gatekeeper.Modules.Provider {
     public class RemoteUserInfoModule : RemoteApplicationModule {
-        public RemoteUserInfoModule(SContext serverContext) : base("/app", serverContext) {
+        public RemoteUserInfoModule(SContext serverContext) : base("/remote", serverContext) {
             Get<RemoteGetUser>("/user", async (req, res) => {
                 await res.Negotiate(new PublicUser(currentUser));
             });
