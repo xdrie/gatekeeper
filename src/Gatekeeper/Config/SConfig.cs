7838984ac88035288c94fc0318a3b98df5745278
xdrie <xdrie@alt.icu>
2020-04-04 17:29:02 -0700

    initialize RemoteApp::layers in config

--- src/Gatekeeper/Config/SConfig.cs
+++ src/Gatekeeper/Config/SConfig.cs
@@ -33,7 +33,7 @@ namespace Gatekeeper.Config {
 
         public class RemoteApp {
             public virtual string name { get; set; }
-            public virtual List<string> layers { get; set; }
+            public virtual List<string> layers { get; set; } = new List<string>();
         }
 
         public List<RemoteApp> apps = new List<RemoteApp> {new GlobalRemoteApp()};
