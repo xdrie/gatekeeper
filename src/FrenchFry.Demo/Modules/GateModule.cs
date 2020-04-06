44437de9130af03e6db2d6d4b0aad447fef5a0f2
xdrie <xdrie@alt.icu>
2020-04-06 02:05:51 -0700

    add /a/auth/login to redirect

--- src/FrenchFry.Demo/Modules/GateModule.cs
+++
@@ -1,8 +0,0 @@
-using Degate.Modules;
-using FrenchFry.Demo.Config;
-
-namespace FrenchFry.Demo.Modules {
-    public class GateModule : GateLinkModule<SContext> {
-        public GateModule(SContext serverContext) : base("/gate", serverContext) { }
-    }
-}
