60b31a7659b68805875f3d426fefc2968d231ba7
xdrie <xdrie@alt.icu>
2020-04-06 15:43:50 -0700

    automatically detect availability of gatekeeper

--- src/FrenchFry.Demo/wwwroot/js/gate_login.js
+++ src/FrenchFry.Demo/wwwroot/js/gate_login.js
@@ -11,7 +11,7 @@ function _gklogin_recieve(config, event) {
     // BEGIN FILE SCOPE
 
     // - constants
-    let gate_login_button = `<a class="btn btn-primary gate-signin-button">\n<img src="$SERVER/img/glyph.png"/>\nlogin\n</a>`
+    let gate_login_button = `<a class="btn btn-primary gate-signin-button">\n<img id="gate-signin-icon" />\nlogin\n</a>`
 
     // - lib
     const $ = document.querySelector.bind(document)
@@ -22,28 +22,45 @@ function _gklogin_recieve(config, event) {
     $load(e => {
         // update all signin sections
         let sign = $('.gate-signin');
-        let config = {
+        let cfg = {
             cb: sign.getAttribute('data-onsuccess'),
             server: sign.getAttribute('data-server'),
             app: sign.getAttribute('data-appid')
         };
-        console.log('configurining gate signin', config);
-        sign.innerHTML = get_login_button(config);
+        console.log('configurining gate signin', cfg);
+        sign.innerHTML = get_login_button(cfg);
 
         // register click event on login button
         let login_btn = $('.gate-signin-button');
         login_btn.addEventListener("click", e => {
-            console.log("requesting login", config);
+            console.log("requesting login", cfg);
 
             // popup gatekeeper link
             let origin = window.location.origin;
-            let gk_window = window.open(`${config.server}/link?app=${config.app}&cb=${origin}&p=1`);
-            
+            let gk_window = window.open(`${cfg.server}/link?app=${cfg.app}&cb=${origin}&p=1`);
+
             // register callback
             window.addEventListener("message",
-                (e) => _gklogin_recieve(config, e),
+                (e) => _gklogin_recieve(cfg, e),
                 false);
         });
+
+        // disable button until load
+        login_btn.setAttribute("disabled", "disabled");
+
+        // load gate signin
+        let signinIcon = $('#gate-signin-icon');
+        var glyph = new Image();
+        glyph.onload = () => {
+            signinIcon.src = glyph.src;
+            login_btn.removeAttribute("disabled");
+        };
+        glyph.onerror = () => {
+            // failed to load glyph
+            console.error("gatekeeper login not available");
+            login_btn.innerText = "[unavailable]";
+        };
+        glyph.src = `${cfg.server}/img/glyph.png`;
     });
 
     function get_login_button(cfg) {
