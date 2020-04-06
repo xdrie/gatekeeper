662fc80c2b9a8885308e50f882e98430e73e474d
xdrie <xdrie@alt.icu>
2020-04-05 22:54:09 -0700

    post token to callback url

--- src/Gatekeeper.Server/wwwroot/js/link.js
+++ src/Gatekeeper.Server/wwwroot/js/link.js
@@ -1,22 +1,19 @@
-
-function addFormField(form, key, value) {
-    var input = document.createElement("input");
-    input.type = "text";
-    input.name = key;
-    input.value = value;
-    form.appendChild(input);
-}
-
-function sendCallback(url, token) {
-    console.log('posting callback to', uri);
+function redirectPost(url, data) {
     var form = document.createElement('form');
-    form.method = 'post';
+    form.method = 'POST';
     form.action = url;
     // add fields
-    addFormField(form, 'content', token.content);
-    addFormField(form, 'expires', token.expires);
-    addFormField(form, 'scope', token.scope);
+    for (var name in data) {
+        var input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = name;
+        input.value = data[name];
+        form.appendChild(input);
+    }
+    console.log('posting to', url, form);
+    document.body.appendChild(form);
     form.submit();
+    document.body.removeChild(form);
 };
 
 function show_link_error(err) {
@@ -41,8 +38,9 @@ async function linkApplication(app, cbUri) {
         console.log(resp);
         // we successfully obtained our app token.
         let appToken = resp.data;
+        console.log('app token', appToken);
         // now, we need to post to the callback uri
-        sendCallback(cbUri, appToken);
+        redirectPost(cbUri, appToken);
     } catch (err) {
         show_link_error(err);
     }
