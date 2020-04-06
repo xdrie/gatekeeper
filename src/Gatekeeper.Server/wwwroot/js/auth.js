e7fc172626fc368785e4c4259e6a0ae414829985
xdrie <xdrie@alt.icu>
2020-04-05 20:04:16 -0700

    store authorized tokens

--- src/Gatekeeper.Server/wwwroot/js/auth.js
+++ src/Gatekeeper.Server/wwwroot/js/auth.js
@@ -15,6 +15,38 @@ function parseFormData(formData) {
     return object;
 }
 
+function storeToken(token) {
+    window.localStorage.setItem('tok', token);
+    console.log('stored token:', token);
+}
+
+function loadToken() {
+    let v = window.localStorage.getItem('tok');
+    console.log('loaded token:', v);
+    return v;
+}
+
+function get_client() {
+    let cfg = {
+        baseURL: '/a',
+        // timeout: 1000,
+        headers: { }
+    };
+    // check if token is saved
+    let token = loadToken();
+    if (token) {
+        cfg.headers['Authorization'] = `Bearer ${token}`;
+    }
+    let client = axios.create(cfg);
+    client.authed = token != null;
+    return client;
+}
+
+function show_error(msg) {
+    $('#error').innerText = msg;
+    $('#error').show()
+}
+
 function show_auth_error(err) {
     console.error(err);
     let msg = '';
@@ -29,14 +61,23 @@ function show_auth_error(err) {
             msg = 'invalid credentials'
             break;
     }
-    $('#error').innerText = msg;
-    $('#error').show()
+    show_error(msg);
+}
+
+// save authorization
+function storeAuthorization(data) {
+    let user = data.user;
+    console.log('authorized as', user);
+    let token = data.token;
+    storeToken(token.content);
 }
 
 async function auth_create(data) {
     try {
-        const resp = await axios.post('/a/auth/create', data);
+        const client = get_client();
+        const resp = await client.post('/auth/create', data);
         console.log(resp);
+        storeAuthorization(resp.data);
         window.location.href = "/verify"; // successfully created account, please verify
     } catch (err) {
         show_auth_error(err);
@@ -45,8 +86,10 @@ async function auth_create(data) {
 
 async function auth_login(data) {
     try {
-        const resp = await axios.post('/a/auth/login', data);
+        const client = get_client();
+        const resp = await client.post('/auth/login', data);
         console.log(resp);
+        storeAuthorization(resp.data);
         window.location.href = "/dash"; // successfully logged in
     } catch (err) {
         show_auth_error(err);
@@ -56,7 +99,12 @@ async function auth_login(data) {
 async function auth_verify(data) {
     try {
         let code = data.code;
-        const resp = await axios.post(`/a/auth/verify/${code}`);
+        const client = get_client();
+        if (!client.authed) {
+            show_error('no token stored');
+            throw "client not authorized";
+        }
+        const resp = await client.post(`/auth/verify/${code}`);
         console.log(resp);
         window.location.href = "/dash"; // successful verify
     } catch (err) {
