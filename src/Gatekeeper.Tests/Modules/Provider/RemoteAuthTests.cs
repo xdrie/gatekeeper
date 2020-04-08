63f443d23874231a9c72ee2abdec42b65d0848f3
xdrie <xdrie@alt.icu>
2020-04-07 19:36:15 -0700

    fix remote auth tests

--- src/Gatekeeper.Tests/Modules/Provider/RemoteAuthTests.cs
+++ src/Gatekeeper.Tests/Modules/Provider/RemoteAuthTests.cs
@@ -1,49 +1,40 @@
-using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Gatekeeper.Models.Identity;
 using Gatekeeper.Tests.Base;
-using Gatekeeper.Tests.Meta;
 using Gatekeeper.Tests.Utilities;
 using Newtonsoft.Json;
 using Xunit;
 
 namespace Gatekeeper.Tests.Modules.Provider {
     public class RemoteAuthTests : UserDependentTests {
+        private RemoteIdentity identity;
+        private HttpClient appClient;
         public RemoteAuthTests(ServerTestFixture fx) : base(fx) { }
 
-        private async Task<RemoteIdentity> getSaltAppIdentity() {
+        public override async Task InitializeAsync() {
+            await base.InitializeAsync();
+
             var resp = await client.GetAsync("/a/app/login/Salt");
             resp.EnsureSuccessStatusCode();
-            var appIdentity = JsonConvert.DeserializeObject<RemoteIdentity>(await resp.Content.ReadAsStringAsync());
-            Assert.Equal("/Food/Salt", appIdentity.token.scope);
-            return appIdentity;
-        }
+            identity = JsonConvert.DeserializeObject<RemoteIdentity>(await resp.Content.ReadAsStringAsync());
+            Assert.Equal("/Food/Salt", identity.token.scope);
+            Assert.Equal(username, identity.user.username);
 
-        private HttpClient getAppClient(Token appToken) {
-            throw new NotImplementedException();
             // authenticate a client on behalf of the application
-            var appClient = fx.getClient();
-            appClient.addBearerToken(appToken);
+            appClient = fx.getClient();
+            appClient.addBearerToken(identity.token);
             appClient.DefaultRequestHeaders.Add(Gatekeeper.Constants.APP_SECRET_HEADER, Constants.Apps.APP_SECRET);
-            return appClient;
         }
 
         [Fact]
         public async Task getRemoteUserInfo() {
-            var identity = await getSaltAppIdentity();
-            var appClient = getAppClient(identity.token);
-
             var resp = await appClient.GetAsync("/a/remote/user");
             resp.EnsureSuccessStatusCode(); // valid user info
         }
 
         [Fact]
         public async Task getRemoteAuthInfo() {
-            var identity = await getSaltAppIdentity();
-            Assert.Equal(username, identity.user.username);
-            var appClient = getAppClient(identity.token);
-
             var resp = await appClient.GetAsync("/a/remote");
             resp.EnsureSuccessStatusCode();
 
